2c3f9f98d8c32b1797089ac808ddb1d4f188056d
chenlizan <clz_318@live.com>
2023-08-02 21:15:53 +0800

    8.2

--- main.py
+++ main.py
@@ -2,28 +2,18 @@ import os
 
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
-from openpyxl.worksheet.table import Table, TableStyleInfo
 from pypdf import PdfReader
 import tabula
 
-HERE = os.path.abspath(os.path.dirname(__file__))
-
-path = os.path.join(HERE,
-                    "files/Bertonia(2021)_Teachers' Preferences for Proximity and the Implications for Staffing Schools.pdf")
-
-# pr = PdfReader(path)
-#
-# print(pr.pages[9].extract_text())
-
 wb = Workbook()
-ws = wb.active
 
-dfs = tabula.read_pdf(path, pages='all', multiple_tables=True)
+HERE = os.path.abspath(os.path.dirname(__file__))
+file_dir = HERE + "\\files"
 
-print(len(dfs))
-
-for df in dfs:
-    for r in dataframe_to_rows(df):
-        ws.append(r)
-
-wb.save("table.xlsx")
+for f in os.listdir(file_dir):
+    ws = wb.active
+    for df in tabula.read_pdf(os.path.join(file_dir, f), pages='all', multiple_tables=True):
+        for r in dataframe_to_rows(df):
+            ws.append(r)
+    wb.save(f + ".xlsx")
+    print(f + ".xlsx" + " saved")
