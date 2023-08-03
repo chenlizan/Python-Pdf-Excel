ad42704a8af95f055af5bec063aee0808a976efb
chenlizan <clz_318@live.com>
2023-08-03 14:15:47 +0800

    8.3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
@@ -8,12 +9,27 @@ import tabula
 wb = Workbook()
 
 HERE = os.path.abspath(os.path.dirname(__file__))
-file_dir = HERE + "\\files"
+file_dir = os.path.join(HERE, "files")
+
+pattern = re.compile("Table\\s[0-9a-zA-Z]+\\.")
+
+dd = pattern.search("190 T. Falch & M. RønningTable 5. Effects of alternative measures on student performance")
 
 for f in os.listdir(file_dir):
+    print("开始分析 " + f + ".xlsx")
+    reader = PdfReader(os.path.join(file_dir, f))
     ws = wb.active
-    for df in tabula.read_pdf(os.path.join(file_dir, f), pages='all', multiple_tables=True):
-        for r in dataframe_to_rows(df):
-            ws.append(r)
+    for paper_index in range(len(reader.pages)):
+        print("#", end="")
+        for df in tabula.read_pdf(os.path.join(file_dir, f), pages=paper_index + 1, multiple_tables=True):
+            title = ""
+            for chars in reader.pages[paper_index].extract_text().splitlines():
+                mo = pattern.search(chars)
+                if mo:
+                    title = chars[mo.start():]
+            ws.append([])
+            ws.append(["第%d页" % (paper_index + 1), title])
+            for r in dataframe_to_rows(df):
+                ws.append(r)
     wb.save(f + ".xlsx")
-    print(f + ".xlsx" + " saved")
+    print("\n" + f + ".xlsx" + " 保存结果\n")
